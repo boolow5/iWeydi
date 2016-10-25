516bf32ee306637002a86e22290e760df97907e0
Mahdi Bolow <boolow5@gmail.com>
2016-10-25 18:23:17 +0300

    added g.InitEnv()

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	_ "github.com/boolow5/iWeydi/routers"
 	"github.com/astaxie/beego"
+	"github.com/boolow5/iWeydi/g"
+	_ "github.com/boolow5/iWeydi/routers"
 )
 
 func main() {
+	g.InitEnv()
 	beego.Run()
 }
-
