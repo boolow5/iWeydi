2d72b0e4786f9c4ffe24e2b8fc9dd555a2cd4d7c
Mahdi Bolow <boolow5@gmail.com>
2016-10-26 14:06:01 +0300

    made some changes for postgres to work

--- g/g.go
+++ g/g.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"os"
 	//_ "github.com/go-sql-driver/mysql"
-
 	"github.com/astaxie/beego/orm"
+	"github.com/boolow5/iWeydi/models"
 	_ "github.com/lib/pq"
 )
 
@@ -34,7 +34,7 @@ func InitEnv() {
 	//orm.RegisterDataBase("default", "mysql", dbLink, maxIdleConn, maxOpenConn)
 
 	orm.RegisterDataBase("default", "postgres", fmt.Sprintf("postgres://%s:%s@%s/%s?port=%i&sslmode=disable",
-		DATABASE_USER, DATABASE_PASS, DATABASE_HOST_NAME, DATABASE_NAME, DATABASE_PORT))
+		DATABASE_USER, DATABASE_PASS, DATABASE_HOST_NAME, DATABASE_PORT, DATABASE_NAME))
 	//orm.RegisterDataBase("default", "postgres", "user="+DATABASE_USER+" password="+DATABASE_PASS+" dbname="+DATABASE_NAME+" sslmode=disable")
 
 	if RUN_MODE == "dev" {
@@ -42,4 +42,11 @@ func InitEnv() {
 	} else {
 		orm.Debug = false
 	}
+
+	/*orm.RegisterModel(new(models.User), new(models.Profile), new(models.Question), new(models.Answer),
+	new(models.Topic), new(models.Feed), new(models.Follower), new(models.Like))*/
+	orm.RegisterModelWithPrefix("weydi_", new(models.User), new(models.Profile), new(models.Question),
+		new(models.Answer), new(models.Topic), new(models.Feed), new(models.Follower), new(models.Like),
+		new(models.QuestionComment), new(models.AnswerComment))
+	//orm.RunCommand()
 }
