e27599c64bf699e22eaba4faec5922f5dc347eea
Mahdi Bolow <boolow5@gmail.com>
2016-10-27 02:21:49 +0300

    added path for put/post/getOne user

--- routers/router.go
+++ routers/router.go
@@ -9,4 +9,5 @@ func init() {
 	beego.Router("/", &controllers.MainController{})
 
 	beego.Router("/api/user", &controllers.UserAPIController{}, "get:Get;post:Post")
+	beego.Router("/api/user/:id", &controllers.UserAPIController{}, "get:GetOne;put:Put")
 }
