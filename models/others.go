36eb1e128f70e893efd3bf5b5ac4429e076efee8
Mahdi Bolow <boolow5@gmail.com>
2016-10-30 21:53:30 +0300

    added some fields

--- models/others.go
+++ models/others.go
@@ -5,14 +5,22 @@ import (
 	"time"
 )
 
+type Language struct {
+	Id   int    `json:"id" orm:"auto"`
+	Name string `json:"name" orm:"size(100);unique"`
+	Code string `json:"code" orm:"size(10)";unique`
+}
+
 type Topic struct {
 	Id        int       `json:"id" orm:"auto"`
 	CreatedAt time.Time `json:"created_at" orm:"auto_now_add;type(datetime)"`
 	UpdatedAt time.Time `json:"updated_at" orm:"auto_now;type(datetime)"`
 
-	Name      string `json:"name" orm:"size(100)"`
-	Followers int    `json:"followers"`
-	Parent    *Topic `json:"parent_topic" orm:"rel(fk);on_delete(cascade)"`
+	Name          string `json:"name" orm:"size(100);unique"`
+	FollowerCount int    `json:"followers"`
+	Parent        *Topic `json:"parent_topic" orm:"rel(fk);on_delete(cascade)"`
+
+	Followers []Follower `json:"followers_ids" orm:"-"`
 }
 
 func (t *Topic) TableName() string {
@@ -37,9 +45,9 @@ func (f *Feed) TableName() string {
 type Follower struct {
 	Id int `json:"id" orm:"auto"`
 
-	Followee *User  `json:"followee" orm:"rel(fk);on_delete(cascade)"`
-	User     *User  `json:"user" orm:"rel(fk);on_delete(cascade)"`
-	Topic    *Topic `json:"topic" orm:"rel(fk);on_delete(cascade)"`
+	Followee *User  `json:"followee" orm:"null;rel(fk);on_delete(cascade)"`
+	User     *User  `json:"user" orm:"null;rel(fk);on_delete(cascade)"`
+	Topic    *Topic `json:"topic" orm:"null;rel(fk);on_delete(cascade)"`
 }
 
 func (f *Follower) TableName() string {
@@ -49,8 +57,9 @@ func (f *Follower) TableName() string {
 type Knows struct {
 	Id int `json:"id" orm:"auto"`
 
-	User  *User  `json:"user" orm:"rel(fk);on_delete(cascade)"`
-	Topic *Topic `json:"knows" orm:"rel(fk);on_delete(cascade)"`
+	User     *User     `json:"user" orm:"null;rel(fk);on_delete(cascade)"`
+	Topic    *Topic    `json:"knows" orm:"null;rel(fk);on_delete(cascade)"`
+	Language *Language `json:"language" orm:"null;rel(fk);on_delete(cascade)"`
 }
 
 func (k *Knows) TableName() string {
@@ -62,7 +71,8 @@ func (k *Knows) String() string {
 }
 
 type Like struct {
-	Id int `json:"id" orm:"auto"`
+	Id      int  `json:"id" orm:"auto"`
+	Postive bool `json:"positive" orm:"positive;default(true)"`
 
 	User            *User            `json:"user" orm:"null;rel(fk);on_delete(cascade)"`
 	Question        *Question        `json:"question" orm:"null;rel(fk);on_delete(cascade)"`
