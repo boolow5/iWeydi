a5c8d85aef23235d8794ca6580646a4bf4859124
Mahdi Bolow <boolow5@gmail.com>
2016-10-30 21:54:14 +0300

    added comments array (hidden from orm)

--- models/questions.go
+++ models/questions.go
@@ -7,10 +7,13 @@ type Question struct {
 	CreatedAt time.Time `json:"created_at" orm:"auto_now_add;type(datetime)"`
 	UpdatedAt time.Time `json:"updated_at" orm:"auto_now;type(datetime)"`
 
-	Text        string            `json:"text" orm:"size(300)"`
-	Description string            `json:"description" orm:"null;size(600)"`
-	Author      *User             `json:"author" orm:"rel(fk);on_delete(cascade)"`
-	Comments    []QuestionComment `json:"comments" orm:"-"`
+	Text        string `json:"text" orm:"unique;size(300)"`
+	Description string `json:"description" orm:"null;size(600)"`
+	Author      *User  `json:"author" orm:"rel(fk);on_delete(cascade)"`
+
+	Language *Language `json:"language" orm:"rel(fk);on_delete(set_null);null"`
+
+	Comments []*QuestionComment `json:"comments" orm:"-"`
 }
 
 func (q *Question) TableName() string {
