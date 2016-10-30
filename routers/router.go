1ea94dec04de734ce345769efc7e534a20f4c2c3
Mahdi Bolow <boolow5@gmail.com>
2016-10-30 21:56:36 +0300

    reorganized some shit

--- routers/router.go
+++ routers/router.go
@@ -6,8 +6,51 @@ import (
 )
 
 func init() {
-	beego.Router("/", &controllers.MainController{})
+	// GENERAL
+	// NORMAL
+	beego.Router("/", &controllers.MainController{}, "*:Home")
+	beego.Router("/about", &controllers.MainController{}, "get:GetAbout")
 
+	// USER AND AUTHS
+	// API
 	beego.Router("/api/user", &controllers.UserAPIController{}, "get:Get;post:Post")
 	beego.Router("/api/user/:id", &controllers.UserAPIController{}, "get:GetOne;put:Put")
+	//beego.Router("/api/user/login", &controllers.UserAPIController{}, "post:Login")
+	//beego.Router("/api/user/logout", &controllers.UserAPIController{}, "get:Logout")
+	// NORMAL
+	beego.Router("/user/profile", &controllers.UserController{}, "get:GetProfile")
+	beego.Router("/user/register", &controllers.UserController{}, "get:Register;post:PostUser")
+	beego.Router("/user/login", &controllers.UserController{}, "get:Login;post:PostLogin")
+	beego.Router("/user/logout", &controllers.UserController{}, "*:Logout")
+
+	// QUESTION
+	// API
+	beego.Router("/question/:id", &controllers.QuestionController{}, "get:GetOneQuestion")
+
+	// NORMAL
+	beego.Router("/questions", &controllers.QuestionController{}, "get:GetQuestions")
+	beego.Router("/question", &controllers.QuestionController{}, "get:AddQuestion")
+
+	// ANSWERS
+	// API
+	// NORMAL
+
+	// TOPICS
+	// API
+	// NORMAL
+
+	// QUESTION
+	// API
+	// NORMAL
+
+	// LIKES
+	// API
+
+	// FOLLOW
+	// API
+
+	// MISC
+	beego.Router("/translate/:word/:args", &controllers.MiscAPIController{}, "*:GetTranslation")
+
+	beego.Router("/api/question", &controllers.QuestionAPIController{}, "post:Post")
 }
