a8c55c0c279197f64bdb81660400da1f3b68b1dd
Mahdi Bolow <boolow5@gmail.com>
2016-10-30 21:16:43 +0300

    added home, about pages

--- controllers/default.go
+++ controllers/default.go
@@ -1,13 +1,15 @@
 package controllers
 
-import "github.com/astaxie/beego"
-
 type MainController struct {
-	beego.Controller
+	BaseController
 }
 
-func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
+func (this *MainController) Home() {
+	SetupCommonLayout("pages/home.tpl", &this.Controller)
+	this.Data["Title"] = "home"
+}
+
+func (this *MainController) GetAbout() {
+	this.Data["Title"] = "About"
+	SetupCommonLayout("pages/about.tpl", &this.Controller)
 }
