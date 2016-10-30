53d66928016e2ea80b3a5f9464ded129d76e8632
Mahdi Bolow <boolow5@gmail.com>
2016-10-30 21:52:03 +0300

    added comments array (hidden from orm)

--- models/answers.go
+++ models/answers.go
@@ -7,9 +7,11 @@ type Answer struct {
 	CreatedAt time.Time `json:"created_at" orm:"auto_now_add;type(datetime)"`
 	UpdatedAt time.Time `json:"updated_at" orm:"auto_now;type(datetime)"`
 
-	Text     string          `json:"text" orm:"size(1000)"`
-	Author   *User           `json:"author" orm:"rel(fk);on_delete(cascade)"`
-	Comments []AnswerComment `json:"comments" orm:"-"`
+	Text   string `json:"text" orm:"size(1000)"`
+	Author *User  `json:"author" orm:"rel(fk);on_delete(cascade)"`
+
+	Question *Question        `json:"question" orm:"rel(fk);on_delete(cascade)"`
+	Comments []*AnswerComment `json:"comments" orm:"-"`
 }
 
 type AnswerComment struct {
