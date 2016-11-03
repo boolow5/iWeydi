d2358b9361e76e64597c2c7cdf2342c6b15ffb0e
Mahdi Bolow <boolow5@gmail.com>
2016-11-04 01:20:37 +0300

    added some more columns

--- models/feed.go
+++ models/feed.go
@@ -8,11 +8,14 @@ type Feed struct {
 	UserId           int       `json:"user_id"`
 	UserFullName     string    `json:"user_full_name"`
 	ActivityTypeName string    `json:"activity_type_name"`
+
+	Question *Question `json:"question"`
+	Answer   *Answer   `json:"answer"`
 }
 
 func ViewName() string {
 	return "ACTIVITY_VIEW"
 }
 func (f *Feed) Columns() []string {
-	return []string{"activity_id", "created_at", "user_id", "user_full_name", "activity_type_name"}
+	return []string{"activity_id", "created_at", "user_id", "user_full_name", "activity_type_name", "question_id", "answer_id"}
 }
