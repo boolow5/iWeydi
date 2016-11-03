539d26dbdb49b552fb75209e965c627d28adb457
Mahdi Bolow <boolow5@gmail.com>
2016-11-03 19:39:28 +0300

    will hold sql code to configure database

---
+++ conf/prepare.sql
@@ -0,0 +1,31 @@
+/*
+  The following SQL code is run the first time to configre the database to behave as intended
+  It contains:
+    1. Table Declarations
+    2. Function Declarations
+    3. Trigger Declarations
+    4. Some data to test the database
+  Table Declarations may change during the development so, we are not going to put it here until
+  we are sure the development cycle is over.
+*/
+
+/*                              1. Table Declarations.                                    */
+/******************************************************************************************/
+
+
+
+
+
+/*                               2. Function Declarations                                 */
+/******************************************************************************************/
+
+
+
+
+/*                               2. Trigger Declarations                                 */
+/******************************************************************************************/
+
+
+
+/*                               2. Data                                                  */
+/******************************************************************************************/
