932f762720df709597f5843206d5e6c8fd5d87df
Mahdi Bolow <boolow5@gmail.com>
2016-11-04 01:19:31 +0300

    added counters to the model

--- models/answers.go
+++ models/answers.go
@@ -10,16 +10,10 @@ type Answer struct {
 	Text   string `json:"text" orm:"size(1000)"`
 	Author *User  `json:"author" orm:"rel(fk);on_delete(cascade)"`
 
-	Question *Question        `json:"question" orm:"rel(fk);on_delete(cascade)"`
-	Comments []*AnswerComment `json:"comments" orm:"-"`
-}
+	Question *Question  `json:"question" orm:"rel(fk);on_delete(cascade)"`
+	Comments []*Comment `json:"comments" orm:"-"`
 
-type AnswerComment struct {
-	Id        int       `json:"id" orm:"auto"`
-	CreatedAt time.Time `json:"created_at" orm:"auto_now_add;type(datetime)"`
-	UpdatedAt time.Time `json:"updated_at" orm:"auto_now;type(datetime)"`
-
-	Text   string  `json:"text" orm:"size(500)"`
-	Author *User   `json:"author" orm:"rel(fk);on_delete(cascade)"`
-	Answer *Answer `json:"answer" orm:"rel(fk);on_delete(cascade)"`
+	LoveCount    int `json:"love_count" orm:"default(0)"`
+	HateCount    int `json:"hate_count" orm:"default(0)"`
+	CommentCount int `json:"comment_count" orm:"default(0)"`
 }
