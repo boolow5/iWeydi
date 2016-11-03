6f4a91383b5a1fbe7bae930d0b1ff00f247981a5
Mahdi Bolow <boolow5@gmail.com>
2016-11-03 04:30:39 +0300

    re-engineered feeds

---
+++ models/feed.go
@@ -0,0 +1,18 @@
+package models
+
+import "time"
+
+type Feed struct {
+	ActivityId       int       `json:"activity_id"`
+	CreatedAt        time.Time `json:"created_at"`
+	UserId           int       `json:"user_id"`
+	UserFullName     string    `json:"user_full_name"`
+	ActivityTypeName string    `json:"activity_type_name"`
+}
+
+func ViewName() string {
+	return "ACTIVITY_VIEW"
+}
+func (f *Feed) Columns() []string {
+	return []string{"activity_id", "created_at", "user_id", "user_full_name", "activity_type_name"}
+}
