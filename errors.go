861e468fd262d0ecfb4b14e59b552c9aa36e6752
Mahdi Bolow <boolow5@gmail.com>
2016-11-06 12:51:03 +0300

    moved code to main.go file

--- errors.go
+++ errors.go
@@ -1,11 +1,1 @@
 package main
-
-import "net/http"
-
-func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("404 - NOT FOUND"))
-}
-
-func InternalErrorHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("500 - INTERNAL ERROR"))
-}
