a45793f37efd9062d46f6b0e84c68d98b0dfc01c
Mahdi Bolow <boolow5@gmail.com>
2016-11-07 14:00:14 +0300

    added route for reaction api controller

--- routers/router.go
+++ routers/router.go
@@ -44,7 +44,7 @@ func init() {
 
 	// LIKES
 	// API
-	beego.Router("/api/reaction/:answer_id", &controllers.ReactionAPIController{}, "get:PostAnswerReaction")
+	beego.Router("/api/reaction/:item_id/:postive", &controllers.ReactionAPIController{}, "get:PostAnswerReaction")
 
 	// FOLLOW
 	// API
