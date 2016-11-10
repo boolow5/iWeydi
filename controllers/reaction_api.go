29dbda7e0c3f6b57d419177cfb62a63e7d0f167c
Mahdi Bolow <boolow5@gmail.com>
2016-11-10 22:57:04 +0300

    fixed a bug related to question reactions

--- controllers/reaction_api.go
+++ controllers/reaction_api.go
@@ -13,6 +13,10 @@ type ReactionAPIController struct {
 	beego.Controller
 }
 
+func (this *ReactionAPIController) Prepare() {
+	this.EnableXSRF = false
+}
+
 func (this *ReactionAPIController) PostAnswerReaction() {
 	fmt.Println("PostReaction:")
 	if !IsAuthenticated(&this.Controller) {
@@ -54,25 +58,31 @@ func (this *ReactionAPIController) PostAnswerReaction() {
 	// 2. insert like with ordirany orm
 
 	//row_count, err := o.Insert(&item_reaction)
-	item_type, err := strconv.Atoi(this.Ctx.Input.Query("ir_t")) // item reaction type: 1. question, 2. answer, 3. comment
-	if err != nil || (item_type < 1 && item_type > 3) {
+	item_type, err := strconv.Atoi(this.Ctx.Input.Param(":item_type")) // item reaction type: 1. question, 2. answer, 3. comment
+	if err != nil || (item_type < 1 || item_type > 3) {
 		this.Data["json"] = map[string]interface{}{"error": "unknown_reaction_item_type", "reason": err}
 		this.ServeJSON()
 		return
 	}
 	var rawsetter orm.RawSeter
+	fmt.Printf("Reaction type: %v \nTarget Item Type: %v\nItem id: %d\n", postive, item_type, item_id)
 	if item_type == 1 {
 		item_reaction.Question = &models.Question{Id: item_id}
-		rawsetter = o.Raw("SELECT insert_like( ?, ? , ?, NULL , NULL )", postive, user_id, item_id)
+		rawsetter = o.Raw("SELECT insert_question_like( ? , ?, ?)", postive, user_id, item_id)
 	} else if item_type == 2 {
 		item_reaction.Answer = &models.Answer{Id: item_id}
-		rawsetter = o.Raw("SELECT insert_like( ?, ? , NULL, ? , NULL)", postive, user_id, item_id)
+		rawsetter = o.Raw("SELECT insert_answer_like( ? , ?, ?)", postive, user_id, item_id)
 	} else if item_type == 3 {
 		item_reaction.Comment = &models.Comment{Id: item_id}
-		rawsetter = o.Raw("SELECT insert_like( ?, ? , NULL, NULL, ? )", postive, user_id, item_id)
+		rawsetter = o.Raw("SELECT insert_comment_like( ? , ?, ?)", postive, user_id, item_id)
 	}
 
-	_, err = rawsetter.Exec()
+	fmt.Println(rawsetter)
+
+	// TODO: check the postgres function to fix answer counter update;
+	r_count, err := rawsetter.Exec()
+	fmt.Println(err)
+	fmt.Println(r_count)
 
 	if err != nil {
 		this.Data["json"] = map[string]interface{}{"error": "couldnt_save_reaction", "reason": err.Error()}
@@ -80,19 +90,29 @@ func (this *ReactionAPIController) PostAnswerReaction() {
 		return
 	}
 	// 3. get sum of likes hates and send it as json
-	hate_love := []orm.Params{}
-	o.Raw("SELECT (SELECT COUNT(id) FROM weydi_user_likes WHERE postive = true AND answer_id = ?) AS love_count, (SELECT COUNT(id) FROM weydi_user_likes WHERE postive = false AND answer_id = ?) AS hate_count;", item_id, item_id).Values(&hate_love)
+	var hate_love []orm.Params
+	var rs orm.RawSeter
 	it := "none"
 	if item_type == 1 {
 		it = "question"
+		rs = o.Raw("SELECT (SELECT COUNT(id) FROM weydi_user_likes WHERE postive = true AND question_id = ?) AS love_count, (SELECT COUNT(id) FROM weydi_user_likes WHERE postive = false AND question_id = ?) AS hate_count;", item_id, item_id)
 	}
 	if item_type == 2 {
 		it = "answer"
+		rs = o.Raw("SELECT (SELECT COUNT(id) FROM weydi_user_likes WHERE postive = true AND answer_id = ?) AS love_count, (SELECT COUNT(id) FROM weydi_user_likes WHERE postive = false AND answer_id = ?) AS hate_count;", item_id, item_id)
 	}
 	if item_type == 3 {
-		it = "answer"
+		it = "comment"
+		rs = o.Raw("SELECT (SELECT COUNT(id) FROM weydi_user_likes WHERE postive = true AND comment_id = ?) AS love_count, (SELECT COUNT(id) FROM weydi_user_likes WHERE postive = false AND comment_id = ?) AS hate_count;", item_id, item_id)
 	}
 
+	rs.Values(&hate_love)
+
+	if len(hate_love) > 0 {
+		this.Data["json"] = map[string]interface{}{"reactions": hate_love[0], "your_reaction_type": postive, "item_type": it}
+		this.ServeJSON()
+		return
+	}
 	this.Data["json"] = map[string]interface{}{"reactions": hate_love, "your_reaction_type": postive, "item_type": it}
 	this.ServeJSON()
 }
