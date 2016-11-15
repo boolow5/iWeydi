8aa933ce480b3639eaf10a8cb71a3f49d45ef48b
Mahdi Bolow <boolow5@gmail.com>
2016-11-16 01:43:19 +0300

    added notifications model

--- models/feed.go
+++ models/feed.go
@@ -19,3 +19,17 @@ func ViewName() string {
 func (f *Feed) Columns() []string {
 	return []string{"activity_id", "created_at", "user_id", "user_full_name", "activity_type_name", "question_id", "answer_id"}
 }
+
+type Notification struct {
+	Id        int       `json:"id" orm:"auto"`
+	CreatedAt time.Time `json:"created_at" orm:"auto_now_add;type(datetime)"`
+	UpdatedAt time.Time `json:"updated_at" orm:"auto_now;type(datetime)"`
+
+	Activity *Activity `json:"Activity" orm:"rel(fk);on_delete(cascade)"`
+	Receiver *User     `json:"receiver" orm:"rel(fk);on_delete(cascade)"`
+	Seen     bool      `json:"seen" orm:"default(false)"`
+}
+
+func (n *Notification) TableName() string {
+	return "user_notifications"
+}
